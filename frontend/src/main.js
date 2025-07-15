3e5d3276f0a8e2a5fe67a269f54c9939cd39ac95
samuelldmj <samuelldmj5@gmail.com>
2025-07-15 14:34:33 +0100

    made some chnages to main file

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,41 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia';
-import App from './App.vue'
-import router from './router'
+// import { createApp } from 'vue'
+// import { createPinia } from 'pinia';
+// import App from './App.vue'
+// import router from './router'
 import './index.css'
+// 
+// const app = createApp(App)
+// app.use(createPinia());
+// app.use(router)
+// app.mount('#app')
+
+
+
+// import { createApp } from 'vue'
+// import { createPinia } from 'pinia'
+// import App from './App.vue'
+// import router from './router'
+// 
+// const app = createApp(App)
+// const pinia = createPinia()
+// 
+// // Order matters! Pinia must be used before router
+// app.use(pinia)
+// app.use(router)
+// 
+// app.mount('#app')
+
+
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import { createMyRouter } from './router'
 
 const app = createApp(App)
-app.use(createPinia());
-app.use(router)
+const pinia = createPinia()
+
+app.use(pinia)
+app.use(createMyRouter())
+
 app.mount('#app')
