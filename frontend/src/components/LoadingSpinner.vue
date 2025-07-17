d8c45c998c6d58ddeab39e20f0566e8872f04f5c
samuelldmj <samuelldmj5@gmail.com>
2025-07-17 18:50:39 +0100

    animated spinner

--- frontend/src/components/LoadingSpinner.vue
+++ frontend/src/components/LoadingSpinner.vue
@@ -1,13 +1,12 @@
 <template>
     <div class="flex justify-center items-center">
-        <div class="animate-spin rounded-full h-8 w-8 border-b-2"
+        <div class="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2"
             :class="{ 'border-blue-500': !themeStore.isDarkMode, 'border-blue-400': themeStore.isDarkMode }"></div>
     </div>
 </template>
 
 <script setup>
 import { useThemeStore } from '@/stores/theme';
-
 const themeStore = useThemeStore();
 </script>
 
